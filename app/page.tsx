aad883dfdca39a91f619d1a8c5965ba529a14de3
William <williamcpereira89@gmail.com>
2024-05-09 11:26:50 -0300

    form

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,7 @@ const IndexPage: React.FC = () => {
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
+      e.preventDefault();
       fetchData();
     }
   };
@@ -74,69 +75,77 @@ const IndexPage: React.FC = () => {
 
   return (
     <div className="p-4">
-      <div className="flex gap-2 flex-col max-w-[1000px] m-auto">
-        <h1 className="text-4xl font-bold text-gray-900 mb-6">
-          Buscador de Abrigados em Canoas
-        </h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetchData();
+        }}
+      >
+        <div className="flex gap-2 flex-col max-w-[1000px] m-auto">
+          <h1 className="text-4xl font-bold text-gray-900 mb-6">
+            Buscador de Abrigados em Canoas
+          </h1>
 
-        <p className="rounded-sm text-sm p-3 bg-yellow-200/50 text-yellow-800 border-l-4 border-yellow-800 font-medium">
-          Se você não achar pelo nome completo, tente pelo primeiro nome, ou
-          nome e apenas um sobrenome e procure nos resultados filtrados. Os
-          voluntários dos abrigos podem ter digitado de forma diferente, procure
-          por algumas variações do nome.
-        </p>
+          <p className="rounded-sm text-sm p-3 bg-yellow-200/50 text-yellow-800 border-l-4 border-yellow-800 font-medium">
+            Se você não achar pelo nome completo, tente pelo primeiro nome, ou
+            nome e apenas um sobrenome e procure nos resultados filtrados. Os
+            voluntários dos abrigos podem ter digitado de forma diferente,
+            procure por algumas variações do nome.
+          </p>
 
-        <label htmlFor="nameInput">Busque pelo nome da pessoa</label>
-        <input
-          id="name"
-          type="text"
-          value={name}
-          placeholder="Digite o nome da pessoa"
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          className="w-full px-3 py-2 rounded-md border-gray-300 shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500"
-        />
-        <div className="flex gap-4 items-center">
-          <button
-            className="flex w-fit px-4 py-2 mt-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
-            onClick={fetchData}
-            disabled={loading}
-          >
-            {loading ? "CARREGANDO..." : "Procurar"}
-          </button>
-          {loading && (
-            <svg
-              className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-400"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
+          <label htmlFor="name">Busque pelo nome da pessoa</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            placeholder="Digite o nome da pessoa"
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            className="w-full px-3 py-2 rounded-md border-gray-300 shadow-sm focus:outline-none focus:border-blue-500 focus:ring-blue-500"
+          />
+          <div className="flex gap-4 items-center">
+            <button
+              type="submit"
+              className="flex w-fit px-4 py-2 mt-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+              // onClick={fetchData}
+              disabled={loading}
             >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                stroke-width="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+              {loading ? "CARREGANDO..." : "Procurar"}
+            </button>
+            {loading && (
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            )}
+          </div>
+          {error && <div className="text-red-500">{error}</div>}
+          {responseData?.data.length >= 0 && (
+            <p className="mt-6 mb-4 px-4">
+              {responseData?.data.length} resultado(s) encontrado(s).{" "}
+              {responseData?.data.length === 0 && (
+                <span className="font-bold">Não perca as experanças!</span>
+              )}
+            </p>
           )}
         </div>
-        {error && <div className="text-red-500">{error}</div>}
-        {responseData?.data.length >= 0 && (
-          <p className="mt-6 mb-4 px-4">
-            {responseData?.data.length} resultado(s) encontrado(s).{" "}
-            {responseData?.data.length === 0 && (
-              <span className="font-bold">Não perca as experanças!</span>
-            )}
-          </p>
-        )}
-      </div>
+      </form>
       {responseData?.data.map(
         (person: { [x: string]: string }[], __index: number) => {
           const mergedPerson = person.reduce((result, currentObject) => {
